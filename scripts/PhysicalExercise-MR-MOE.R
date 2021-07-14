e060b24697cfc7a10610b2ad2b3e971891f2ec5e
Jahnvi Patel <pateljahnvi30@gmail.com>
2021-07-14 13:44:38 -0400

    Update PhysicalExercise-MR-MOE.R

--- scripts/PhysicalExercise-MR-MOE.R
+++ scripts/PhysicalExercise-MR-MOE.R
@@ -1,7 +1,10 @@
 library(devtools)
 library(tidyverse)
 library(TwoSampleMR)
-
+library(ggplot2)
+library(ggnewscale)
+install.packages("openxlsx",dependencies = TRUE)
+library(openxlsx)
 #EXPOSURE DATA ---------------------------------------------
 physicalactivity.file <- "~/Desktop/Klimentidis2018mvpa.chrall.CPRA_b37.tsv"
 physicalactivity_dat <- read_exposure_data(
@@ -58,6 +61,10 @@ res_moe.pa <- mr_moe(res.pa,rf)
 res.mr <- mr(physicalactivity.dat) #each MR method for each combination of exposure-outcome traits
 generate_odds_ratios(res.mr)
 
+res <- do.call(rbind, res_moe.pa)
+class(res$estimates)
+write_xlsx(res_moe.pa[[1]],"\\Desktop\\Physical Exercise MR-MOEahhhh.xlsx")
+write.csv(res_moe,glue("~/{out.file}/{exposure.summary}_{outcome.summary}.csv"))
 #MAKE PLOTS ------------------------------------------------------------
 #create a scatter plot
 res.pa <- mr(physicalactivity.dat, 
@@ -74,12 +81,27 @@ p1[[1]]
 ggsave(p1[[1]], file="physicalactivityscatterplot.png", width=7, height=7)
 
 b<-c(0,0,0,0,-2.842025,0,0,0,0,0,0,0,-2.842025,0,0,0,0,0,0,0,-2.842025,0,0,0,0,0,0,0,-2.842025,0,0,-9.373453,-9.373453)
-res_moe.pa$lXWUfu.VcdeTs$estimates$a=b
+res_moe.pa$gEOyNc.6cf5D1$estimates$a=b
 
-q <- mr_scatter_plot2(mrdat=physicalactivity.dat,res=res_moe.pa$lXWUfu.VcdeTs$estimates)
-f <- q$data
+q <- mr_scatter_plot2(mrdat=physicalactivity.dat,res=res_moe.pa$gEOyNc.6cf5D1$estimates)
+q[[1]]
+q$plot_env$d$remove=c(FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE,FALSE,TRUE)
+q$plot_env$d$action=c(2,2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,2,2,1)
+z <- q$plot_env$d[-c(10,13)]
+z <- z[-c(10,13),]
+q$plot_env$d=z
 q + facet_wrap(vars(selection)) + theme(legend.position = 'top') 
 
+scatter.tophits.pa <- mr_scatter_plot(res.pa,res.filter2.pa)
+scatter.tophits.pa.a <- scatter.tophits.pa[[1]] + theme_bw() + theme(legend.position = 'bottom')
+
+scatter.steiger.pa <- mr_scatter_plot(res.pa,(slice(res.filter2.pa,-c(13,18))))
+scatter.steigera.pa <- scatter.steiger.pa[[1]] + theme_bw() + theme(legend.position = 'bottom') #steiger
+
+scatter.both.pa <- mr_scatter_plot(res.pa,(slice(res.filter2.pa,-c(13,18))))
+scatter.botha.pa <- scatter.both.pa[[1]] + theme_bw() + theme(legend.position = 'bottom') #both
+
+ggpubr::ggarrange(scatter.tophits.pa.a, scatter.steigera.pa, scatter.botha.pa,common.legend = T)
 #create a forest plot
 res_single_pa <- mr_singlesnp(physicalactivity.dat, 
                            all_method=c(
@@ -106,7 +128,7 @@ p3[[1]]
 ggsave(p3[[1]], file="physicalactivityfunnelplot.png", width=7, height=7)
 
 res.filter.pa <- res_single.pa %>% slice(1:19)
-res_moe2.pa <- subset(res_moe.pa$lXWUfu.VcdeTs$estimates,select = -c(nsnp,ci_low,ci_upp,steiger_filtered,outlier_filtered,selection,method2,MOE,a))
+res_moe2.pa <- subset(res_moe.pa$gEOyNc.6cf5D1$estimates,select = -c(nsnp,ci_low,ci_upp,steiger_filtered,outlier_filtered,selection,method2,MOE))
 res_methods.pa <- paste0("All - ",res_moe2.pa$method)
 res_moe2.pa <- subset(res_moe2.pa,select=-c(method))
 res_moe2.pa <- cbind(res_methods.pa,res_moe2.pa)
@@ -127,6 +149,7 @@ res.filter2.pa <- rbind(res.filter.pa,res_moe_filter.pa)
 
 tophits.pa <- mr_funnel_plot(res.filter2.pa)
 tophits.pa.a <- tophits.pa[[1]] + theme_bw() + theme(legend.position = 'bottom')
+
 
 steiger.pa <- mr_funnel_plot(slice(res.filter2.pa,-c(13,18)))
 steigera.pa <- steiger.pa[[1]] + theme_bw() + theme(legend.position = 'bottom') #steiger
